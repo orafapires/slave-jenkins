fdada4efd90953ba01e9752e1e6f78ffc71d65b6
Rafael Pires <rafael.pires@concretesolutions.com.br>
2015-09-02 13:55:10 -0300

    Ajustando o close no ssh

--- slave-status.sh
+++ slave-status.sh
@@ -17,8 +17,7 @@ for JENKINS_NODENAME in `cat $SLAVEFILE`
 do
 	CHECKED=`curl --silent $JENKINS_URL/computer/$JENKINS_NODENAME/api/json | grep -Po '"offline":true' | cut -d':' -f2`
 	if [[ "$CHECKED" == "true" ]]; then
-        	ssh -tt $JENKINS_NODENAME './kill-slave.sh'
-        	exit 0
+        	ssh -tt $JENKINS_NODENAME './kill-slave.sh && exit 0'
 				else
 					echo "INFO: $JENKINS_NODENAME is online, no need to restart."
 	fi
